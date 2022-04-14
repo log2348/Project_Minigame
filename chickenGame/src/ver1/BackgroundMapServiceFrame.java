7021d5ed2c0e74b598833f6e7101531bc1a984db
sodamkimkim <100888879+sodamkimkim@users.noreply.github.com>
2022-04-14 21:56:12 +0900

    소담

--- chickenGame/src/ver1/BackgroundMapServiceFrame.java
+++ chickenGame/src/ver1/BackgroundMapServiceFrame.java
@@ -38,72 +38,67 @@ public class BackgroundMapServiceFrame implements Runnable {
 			int pWidth = player.getWidth();
 			int pHeight = player.getHeight();
 
-			Color leftColor = new Color(serviceImg.getRGB(player.getX() - 10, player.getY() + 40));
-			int leftColorInt = serviceImg.getRGB(player.getX()  - 10, player.getY() + 40);
+			Color leftColor = new Color(serviceImg.getRGB(player.getX() + 10, player.getY() + 40));
+			int leftColorInt = serviceImg.getRGB(player.getX() + 10, player.getY() + 40);
 
-			Color rightColor = new Color(serviceImg.getRGB(player.getX() + pWidth, player.getY() + 10));
-			int rightColorInt = serviceImg.getRGB(player.getX() + pWidth, player.getY() + 10);
+			Color rightColor = new Color(serviceImg.getRGB(player.getX() + pWidth, player.getY() + 40));
+			int rightColorInt = serviceImg.getRGB(player.getX() + pWidth, player.getY() + 40);
 
 			int topColorInt = serviceImg.getRGB(player.getX() + 20, player.getY())
-					+ serviceImg.getRGB(player.getX() + 50 - 20, player.getY());
+					+ serviceImg.getRGB(player.getX() + 55 - 20, player.getY());
 
 			int bottomColorInt = serviceImg.getRGB(player.getX() + 20, player.getY() + pHeight - 13)
-					+ serviceImg.getRGB(player.getX() + 50 - 20, player.getY() + pHeight - 13);
+					+ serviceImg.getRGB(player.getX() + 55 - 20, player.getY() + pHeight - 13);
 
 			// 외벽 및 바닥충돌
 			System.out.println("===========================");
-//			System.out.println(pWidth + "," + pHeight);
-//			System.out.println("leftColor: " + leftColor);
-//			System.out.println("leftColorInt: " + leftColorInt);
-//			System.out.println("rightColor: " + rightColor);
-//			System.out.println("rightColorInt: " + rightColorInt);
-//
-////			System.out.println("bottomColor: " + bottomColor);
-//			System.out.println("bottomColorInt: " + bottomColorInt);
-////			System.out.println("topColor: " + topColor);
-//			System.out.println("topColorInt: " + topColorInt);
+			System.out.println(pWidth + "," + pHeight);
+			System.out.println("leftColor: " + leftColor);
+			System.out.println("leftColorInt: " + leftColorInt);
+			System.out.println("rightColor: " + rightColor);
+			System.out.println("rightColorInt: " + rightColorInt);
 
-			System.out.print("left Color : " + leftColor.getRed() + " ");
-			System.out.print("left Color : " + leftColor.getGreen()+ " ");
-			System.out.print("left Color : " + leftColor.getBlue()+ " ");
-			
-			if(leftColorInt != -1 ) {
+//			System.out.println("bottomColor: " + bottomColor);
+			System.out.println("bottomColorInt: " + bottomColorInt);
+//			System.out.println("topColor: " + topColor);
+			System.out.println("topColorInt: " + topColorInt);
+
+//			System.out.print("left Color : " + leftColor.getRed() + " ");
+//			System.out.print("left Color : " + leftColor.getGreen()+ " ");
+//			System.out.print("left Color : " + leftColor.getBlue()+ " ");
+
+			if (leftColorInt != -1) {
 				System.out.println("왼쪽벽에 충돌했어");
-//				player.setLeftWallCrash(true);
-//				player.setLeft(false);
+				player.setLeftWallCrash(true);
+				player.setLeft(false);
+			} else if (rightColorInt != -1) {
+				System.out.println("오른쪽 벽에 충돌했어");
+				player.setRightWallCrash(true);
+				player.setRight(false);
+
+			} else {
+				player.setLeftWallCrash(false);
+				player.setRightWallCrash(false);
 			}
-			
-			
-//			if (bottomColorInt != -2) { // 바닥흰색배경이 아니면
-//
-//				System.out.println("바닥과 닿았어");
-//				player.setBottomCrash(true);
-//
-//				player.setDown(false);
-//				player.setJumpDownInKit(false);
-//
-//			} else if (topColorInt != -2) { // 천장흰색아니면
-//				System.out.println("천장과 닿았어");
-//				player.setTopCrash(true); 
-//				player.setUp(false);
-//				player.setJumpUpInKit(false);
-//
-//			} else if (leftColorInt != -1) {
-//				System.out.println("왼쪽벽에 충돌했어");
-//				player.setLeftWallCrash(true);
-//				player.setLeft(false);
-//				
-//			} else if (rightColorInt != -1) {
-//				System.out.println("오른쪽 벽에 충돌했어");
-//				player.setRightWallCrash(true);
-//				player.setRight(false);
-//
-//			} else {
-//				player.setLeftWallCrash(false);
-//				player.setRightWallCrash(false);
-//				player.setTopCrash(false);
-//				player.setBottomCrash(false);
-//			}
+
+			if (bottomColorInt != -2) { // 바닥흰색배경이 아니면
+
+				System.out.println("바닥과 닿았어");
+				player.setBottomCrash(true);
+
+				player.setDown(false);
+				player.setJumpDownInKit(false);
+
+			} else if (topColorInt != -2) { // 천장흰색아니면
+				System.out.println("천장과 닿았어");
+				player.setTopCrash(true);
+				player.setUp(false);
+				player.setJumpUpInKit(false);
+
+			} else {
+				player.setTopCrash(false);
+				player.setBottomCrash(false);
+			}
 
 			try {
 				Thread.sleep(1);// thread가 길면.. check해야하는애가 기다리고 있어서 컬러값 check못하고 떨어짐
